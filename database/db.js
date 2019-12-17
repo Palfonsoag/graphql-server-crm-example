ad5ebc47e2163608a37f786ab9338705f98f4afe
Pedro Alfonso <p.alfonsoag@gmail.com>
2019-12-17 14:39:49 -0400

    some other stuff

--- database/db.js
+++ database/db.js
@@ -8,7 +8,7 @@ const clientsSchema = new mongoose.Schema({
   name: String,
   lastName: String,
   company: String,
-  emails: Array,
+  email: String,
   age: Number,
   clientType: String,
   orders: Array
