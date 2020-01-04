a6eeec568477a9ec7b983d5a6cbfc491a44d8037
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-01-03 21:36:06 -0400

    verifying the jwt

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import { typeDefs } from "./database/schema";
 import { resolvers } from "./database/resolvers";
 
+dotenv.config({ path: "variables.env" });
+
 const app = express();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const token = req.headers["authorization"];
+    console.log(token);
+    if (token !== "null") {
+      try {
+        const currentUser = await jwt.verify(token, process.env.SECRET);
+        console.log(currentUser);
+        req.currentUser = currentUser;
+        return { currentUser };
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+});
 
 server.applyMiddleware({ app });
 
